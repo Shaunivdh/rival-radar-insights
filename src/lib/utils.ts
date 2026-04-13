b9efa2c396d0dda6ee4ae52b1c5a0be289317fbc
Shaunivdh <shaunivanderhorst@gmail.com>
2026-04-13 17:47:19 +0100

    chore: add valid postcode check

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isValidUKPostcode(postcode: string): boolean {
+  return /^[A-Z]{1,2}[0-9][0-9A-Z]?\s*[0-9][A-Z]{2}$/i.test(postcode.trim());
+}
